066353b337542d8e2215900dc5d3e81b7c3db079
afhare <alexandra.f.hare@gmail.com>
2019-08-12 17:00:34 -0500

    Fixed minor errors in state-data.csv and state controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,7 +2,7 @@ class StatesController < ApplicationController
     def index
         states = State.all 
         render json: states.to_json(:include => {
-            :representatives => {:only => [:id, :name, :chamber, :role]}, :senators => {:only => [:id, :name, :chamber, :role]}
+            :representatives => {:only => [:id, :name, :chamber, :role, :party]}, :senators => {:only => [:id, :name, :chamber, :role, :party]}
             }, except: [:created_at, :updated_at])
     end
 
@@ -10,7 +10,7 @@ class StatesController < ApplicationController
         state = State.find_by(id: params[:id])
         if state
             render json: state.to_json(:include => {
-                :representatives => {:only => [:id, :name, :chamber, :role]}, :senators => {:only => [:id, :name, :chamber, :role]}
+                :representatives => {:only => [:id, :name, :chamber, :role, :party]}, :senators => {:only => [:id, :name, :chamber, :role, :party]}
                 }, except: [:created_at, :updated_at])
         else
             render json: { message: 'State not found'}
